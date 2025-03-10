5ab60e43add44cf70bee7ae1626845e390f4e767
SendMeMyBread <79381136+SendMeBread@users.noreply.github.com>
2025-03-10 15:57:38 +0000

    Fun

---
+++ battleBots.lua
@@ -0,0 +1,14 @@
+local robot = require("robot")
+local component = require("component")
+local event = require("event")
+nav = component.navigation
+ms = component.motion_sensor
+ms.setSensitivity(0.5)
+while true do
+    pos = nav.getPosition()
+    local _, address, x, y, z, name = event.pull(ms)
+    local entity = world.getEntityAt(pos.x + x, pos.y + y, pos.z + z)
+    if entity and entity.isHostile() then
+        robot.useItem()
+    end
+end
