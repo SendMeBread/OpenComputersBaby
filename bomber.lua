05bd75b46d1b8ea3228bf1055f80e4940c6ce511
SendMeMyBread <79381136+SendMeBread@users.noreply.github.com>
2025-03-10 20:38:23 -0400

    Update bomber.lua

--- bomber.lua
+++ bomber.lua
@@ -8,10 +8,11 @@ while ( slot < 16 )
 do
   for r 0,robot.space(slot),1
   do
+    robot.select(slot)
     robot.placeDown(0)
-    robot.up()
+    robot.select(16)
+    robot.useDown(0)
+    robot.forward()
   end
   slot = slot + 1
 end
-robot.select(16)
-robot.useDown()
