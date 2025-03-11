4d62cccc11a8b16dd543fb0d15c67256cbd25230
SendMeMyBread <79381136+SendMeBread@users.noreply.github.com>
2025-03-11 15:18:03 -0400

    Update battleBots.lua

--- battleBots.lua
+++ battleBots.lua
@@ -21,8 +21,8 @@ while true do
     pos = nav.getPosition()
     mobs = radar.getMobs()
     for k,v in pairs(mobs) do
-        if isMobHostile(pos.x+v[1], pos.y+v[2], pos.z+v[3]) then
-            robot.move(pos.x+v[1]-1, pos.y+v[2], pos.z+v[3])
+        if isMobHostile(pos.x+k.x, pos.y+k.y, pos.z+k.z) then
+            robot.move(pos.x+k.x-1, pos.y+k.y, pos.z+k.z)
             robot.swing()
         end
     end
