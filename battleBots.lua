e47f80889373be9b10e6bdb63f955083acfa77f4
SendMeMyBread <79381136+SendMeBread@users.noreply.github.com>
2025-03-11 14:52:15 -0400

    Update battleBots.lua
    Attempt IG

--- battleBots.lua
+++ battleBots.lua
@@ -1,14 +1,26 @@
 local robot = require("robot")
 local component = require("component")
-local event = require("event")
-nav = component.navigation
-ms = component.motion_sensor
-ms.setSensitivity(0.5)
+local radar = require("component").radar
+
+local hostileMobs = {"minecraft:zombie", "minecraft:skeleton", "minecraft:creeper", "minecraft:spider", "minecraft:witch", "minecraft:slime", "minecraft:cave_spider", "minecraft:zombie_villager", "minecraft:husk", "minecraft:silverfish", "minecraft:stray", "minecraft:evocation_illager", "minecraft:vindication_illager", "minecraft:illusion_illager", "minecraft:vex", "minecraft:guardian", "minecraft:elder_guardian", "minecraft:blaze", "minecraft:ghast", "minecraft:magma_cube", "minecraft:zombie_pigman", "minecraft:wither_skeleton", "minecraft:wither", "minecraft:enderman", "minecraft:endermite", "minecraft:shulker", "minecraft:ender_dragon"}
+
+function isMobHostile(x, y, z)
+    local entity = world.getEntity(x, y, z)
+    if entity then
+        local entityType = entity.getEntityType()
+
+        for _, hostileType in ipairs(hostileMobs) do
+            if entityType == hostileType then
+                return true
+            end
+        end
+    end
+    return false
+end
 while true do
     pos = nav.getPosition()
-    local address, x, y, z, name = event.pull(motion)
-    local entity = world.getEntityAt(pos.x + x, pos.y + y, pos.z + z)
-    if entity and entity.isHostile() then
-        robot.useItem()
-    end
-end
+    mobs = radar.getMobs()
+    for k,v in pairs(mobs) do
+        if isMobHostile(pos.x+k.x, pos.y+k.y, pos.z+k.z) then
+        robot.move(pos.x+k.x-1, pos.y+k.y, pos.z+k.z)
+        robot.swing()
