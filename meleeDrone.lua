952ab405656f778cbcd0896880667edf5d3ca2cd
SendMeMyBread <79381136+SendMeBread@users.noreply.github.com>
2025-03-12 14:56:11 -0400

    Update meleeDrone.lua

--- meleeDrone.lua
+++ meleeDrone.lua
@@ -1,2 +1,42 @@
 drone = component.proxy(component.list('drone')())
 nav = component.proxy(component.list('navigation')())
+event = component.proxy(component.list('event')())
+ms = component.proxy(component.list('motion_sensor')())
+ms.setSensitivity(0.2)
+local hostileMobs = {
+  "minecraft:blaze",
+  "minecraft:cave_spider", "minecraft:creeper",
+  "minecraft:elder_guardian", "minecraft:ender_dragon", "minecraft:enderman", "minecraft:endermite", "minecraft:evocation_illager,
+  "minecraft:ghast", "minecraft:giant", "minecraft:guardian",
+  "minecraft:husk",
+  "minecraft:illusion_illager",
+  "minecraft:magma_cube",
+  "minecraft:shulker", "minecraft:silverfish", "minecraft:skeleton", "minecraft:slime", "minecraft:spider", "minecraft:stray",
+  "minecraft:vex", "minecraft:vindication_illager",
+  "minecraft:witch", "minecraft:wither", "minecraft:wither_skeleton",
+  "minecraft:zombie", "minecraft:zombie_pigman", "minecraft:zombie_villager"
+}
+function isMobHostile(x, y, z)
+  local entity = world.getEntity(x, y, z)
+  if entity ~= nil then
+    local entityType = entity.getEntityType()
+
+    for _, hostType in ipairs(hostileMobs) do
+      if entityType == hostType then
+        return true
+      end
+    end
+  else
+    return false
+  end
+end
+
+function attackMob(is_motion, ms_x, ms_y, ms_z)
+  print(is_motion)
+  pos = nav.getPosition()
+  if isMobHostile(ms_x+pos.x, ms_y+pos.y, ms_z+pos.z) then
+    drone.move(ms_x+pos.x, ms_y+pos.y, ms_z+pos.z)
+    drone.swing()
+  end
+end
+event.listen('motion', attackMob)
