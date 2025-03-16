de671fe53a6f583ca497f16dcd5f66db84a0257e
SendMeMyBread <79381136+SendMeBread@users.noreply.github.com>
2025-03-16 15:15:27 -0400

    Create archerBot_FiringLine.lua

---
+++ archerBot_FiringLine.lua
@@ -0,0 +1,7 @@
+local comp = require("component")
+local robot = require("robot")
+local event = require("event")
+local radar = comp.radar
+local gen = comp.generator
+local nav = comp.navigation
+local inventory = comp.inventory_controller
